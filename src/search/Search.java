d5a1218c48e99df7222b5e13083f0abad3c54261
frebib <frebib@gmail.com>
2015-03-01 00:51:57 +0000

    Added comments and general tidying

--- src/search/Search.java
+++ src/search/Search.java
@@ -20,19 +20,24 @@ public class Search {
 		Map<Node<A>, Node<A>> visited = new LinkedHashMap<Node<A>, Node<A>>();
 		Node<A> node = null;
 
-		frontier.add(start);						// Adds the node to the frontier in the manner specified by the data structure
+		if (p.holds(start.getContents()))
+			return new Nothing<>();
+
+		frontier.add(start);
 		while (!frontier.isEmpty()) {
-			node = frontier.getHead();				// Get and remove the first element in the manner specified by the data structure
-			if (p.holds(node.getContents())) {
+			node = frontier.getHead();
+			if (p.holds(node.getContents())) {		// At this point we reconstruct the path followed from the visited Map
+
 				visited.put(start, null);			// Add start Node as it will be first element in list (last one to be added)
 
 				IList<Node<A>> list = new Nil<>();
-				while (node != null) {
-					list = new Cons<>(node, list);
-					node = visited.get(node);
-				}
+				while (node != null) {				// Iterate through the nodes in the visited map
+					list = new Cons<>(node, list);	// Add the current node to the resulting path
+					node = visited.get(node);		// Get the parent of the node from the Key-Value
+				}									// pair in the Map using the node as the key
 
-				return new Just<>(list);
+				assert (list.size() > 1);			// It should never be that the only node in the list
+				return new Just<>(list);			// is the start node; that should catch at the start.
 			}
 			else
 				for (Node<A> suc : node.getSuccessors())
@@ -43,7 +48,6 @@ public class Search {
 		}
 		return new Nothing<>();
 	}
-
 	public static <A, B extends DataStructure<Node<A>>> Maybe<Node<A>> findNodeFrom(Node<A> start, Predicate<A> p, B frontier) {
 		Set<Node<A>> visited = new HashSet<Node<A>>();
 		Node<A> node;
@@ -56,8 +60,8 @@ public class Search {
 			else
 				for (Node<A> suc : node.getSuccessors())
 					if (!visited.contains(suc)) {
-						frontier.add(suc);
-						visited.add(suc);
+						frontier.add(suc);			// Add all successors to the frontier set so they can be searched
+						visited.add(suc);			// on a later iteration of this while loop
 					}
 		}
 		return new Nothing<>();
