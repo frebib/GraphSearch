14c928c723f389018f7d664181d0b6c1ccacafb0
frebib <frebib@gmail.com>
2015-03-04 02:14:04 +0000

    Fixed a bug with Node equality not Coordinate equality in Search

--- src/search/Search.java
+++ src/search/Search.java
@@ -36,7 +36,7 @@ public class Search {
 		Set<Node<A>> visited = new HashSet<Node<A>>();
 		Node<A> node = null;
 
-		if (start.equals(goal))
+		if (start.contentsEquals(goal.contents))
 			return new Nothing<>();
 
 		start.contents.setCost(0);
@@ -45,7 +45,7 @@ public class Search {
 
 		while (!frontier.isEmpty()) {
 			node = frontier.getHead();				// Get and remove the first element in the manner specified by the data structure
-			if (node.equals(goal))
+			if (node.contentsEquals(goal.contents))
 				return new Just<Node<A>>(node);		// Return found goal Node
 			else
 				for (Node<A> suc : node.getSuccessors())
@@ -72,7 +72,7 @@ public class Search {
 		Map<Node<A>, Node<A>> visited = new LinkedHashMap<Node<A>, Node<A>>();
 		Node<A> node = null;
 
-		if (start.equals(goal))
+		if (start.contentsEquals(goal.contents))
 			return new Nothing<>();
 
 		start.contents.setCost(0);
@@ -81,18 +81,18 @@ public class Search {
 
 		while (!frontier.isEmpty()) {
 			node = frontier.getHead();
-			if (node.equals(goal)) {		// At this point we reconstruct the path followed from the visited Map
+			if (node.contentsEquals(goal.contents)) {	// At this point we reconstruct the path followed from the visited Map
 
-				visited.put(start, null);			// Add start Node as it will be first element in list (last one to be added)
+				visited.put(start, null);				// Add start Node as it will be first element in list (last one to be added)
 
 				IList<Node<A>> list = new Nil<>();
-				while (node != null) {				// Iterate through the nodes in the visited map
-					list = new Cons<>(node, list);	// Add the current node to the resulting path
-					node = visited.get(node);		// Get the parent of the node from the Key-Value
-				}									// pair in the Map using the node as the key
+				while (node != null) {					// Iterate through the nodes in the visited map
+					list = new Cons<>(node, list);		// Add the current node to the resulting path
+					node = visited.get(node);			// Get the parent of the node from the Key-Value
+				}										// pair in the Map using the node as the key
 
-				assert (list.size() > 1);			// It should never be that the only node in the list
-				return new Just<>(list);			// is the start node; that should catch at the start.
+				assert (list.size() > 1);				// It should never be that the only node in the list
+				return new Just<>(list);				// is the start node; that should catch at the start.
 			}
 			else
 				for (Node<A> suc : node.getSuccessors())
