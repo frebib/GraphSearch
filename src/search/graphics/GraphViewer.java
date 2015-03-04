326145f055e03ca412d1a901eb12fdb45dfc76cb
frebib <frebib@gmail.com>
2015-03-04 01:26:18 +0000

    Added a GraphViewer class to encapsulate creating and displaying the form

---
+++ src/search/graphics/GraphViewer.java
@@ -0,0 +1,22 @@
+package search.graphics;
+
+import ilist.IList;
+import search.graph.Coordinate;
+import search.graph.Node;
+
+import javax.swing.JFrame;
+
+public class GraphViewer {
+	public static void traversePath(IList<Node<Coordinate>> path, int nodeDelay) {
+		Traverser traverser = new Traverser();
+		View v = new View(traverser);
+
+		JFrame frame = new JFrame("Graph");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(900, 600);
+		frame.add(v);
+		frame.setVisible(true);
+
+		traverser.runTraversal(path, nodeDelay);
+	}
+}
