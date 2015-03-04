39d1cc77c18d92b7e3c5afc3fb9f0be2ce59dc51
frebib <frebib@gmail.com>
2015-03-04 01:23:55 +0000

    Allowed SearchPriorityQueue to accept a comparator in the constructor

--- src/search/datastructures/SearchPriorityQueue.java
+++ src/search/datastructures/SearchPriorityQueue.java
@@ -1,5 +1,6 @@
 package search.datastructures;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -9,8 +10,11 @@ import java.util.PriorityQueue;
  * @param <A> @inheritDoc
  */
 public class SearchPriorityQueue<A> extends PriorityQueue<A> implements DataStructure<A> {
-
 	private static final long serialVersionUID = 2623101708315097645L;
+
+	public SearchPriorityQueue(Comparator<A> comparer) {
+		super(comparer);
+	}
 
 	@Override
 	public A getHead() {
