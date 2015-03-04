0d996a07bc36fa8e4426e954a4ac65c065c376ad
frebib <frebib@gmail.com>
2015-03-04 01:25:27 +0000

    Node/NodeComparer also follows the <A extends Heuristic> rule

--- src/search/graph/Node.java
+++ src/search/graph/Node.java
@@ -1,43 +1,32 @@
 package search.graph;
 
+import search.Heuristic;
+
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
-public class Node<A> {
-	private A contents;
-	private double f;
+public class Node<A extends Heuristic<A>> {
 	private final Collection<Node<A>> successors;
+	public A contents;
 
 	public Node(A contents) {
 		this.contents = contents;
 		this.successors = new LinkedHashSet<Node<A>>();
-		this.f = 0.0;
 	}
 
 	public void addSuccessor(Node<A> s) {
 		successors.add(s);
 	}
-
 	public Collection<Node<A>> getSuccessors() {
 		return successors;
 	}
-
-	public A getContents() {
-		return contents;
+	public float getF() {
+		return contents.getF();
 	}
 
 	public boolean contentsEquals(A c) {
 		return contents.equals(c);
 	}
-
-	public double getF() {
-		return f;
-	}
-
-	public void setF(double f) {
-		this.f = f;
-	}
-
 	@Override
 	public String toString() {
 		return "Node" + contents.toString();
