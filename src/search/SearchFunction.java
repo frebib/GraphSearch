d340b1f38e4d8faa7eb54e6e6e77707491d5b673
frebib <frebib@gmail.com>
2015-03-04 01:21:59 +0000

    Added SearchFunction interface that extends Function2 for more compact code

---
+++ src/search/SearchFunction.java
@@ -0,0 +1,12 @@
+package search;
+
+import maybe.Function2;
+import search.graph.Coordinate;
+import search.graph.Node;
+
+public interface SearchFunction<A extends Heuristic<A>> extends Function2<Node<A>, Node<A>, Float> {
+	public static final SearchFunction<Coordinate> euclidean = (a, b) -> (float) Math.abs(a.contents.x - b.contents.x) + Math.abs(a.contents.y - b.contents.y);
+	public static final SearchFunction<Coordinate> manhattan = (a, b) -> (float) Math.sqrt(Math.pow(a.contents.x - b.contents.x, 2) + Math.pow(a.contents.y - b.contents.y, 2));
+
+	public Float apply(Node<A> a, Node<A> b);
+}
